af5f2874ffee8670ef6827184bce66c3a44ac4f2
molystechie <molystechie@gmail.com>
2023-03-22 18:00:52 +0300

    Installed cloudflare plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: cloudflare()
 } // site: 'https://molystechie.github.io',
 // base: '/demo_hosting'
 );
